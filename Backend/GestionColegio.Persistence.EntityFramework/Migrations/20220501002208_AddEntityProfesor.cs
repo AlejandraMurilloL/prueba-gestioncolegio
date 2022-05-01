9f0b34a36610d6928b774ffdf75b35f7b11ff987
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-04-30 19:22:58 -0500

    Se agrega migration para entidad profesor

---
+++ Backend/GestionColegio.Persistence.EntityFramework/Migrations/20220501002208_AddEntityProfesor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestionColegio.Persistence.EntityFramework.Migrations
+{
+    public partial class AddEntityProfesor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Profesores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Identificacion = table.Column<string>(nullable: true),
+                    Nombre = table.Column<string>(nullable: true),
+                    Apellido = table.Column<string>(nullable: true),
+                    Edad = table.Column<int>(nullable: true),
+                    Direccion = table.Column<string>(nullable: true),
+                    Telefono = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profesores", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Profesores");
+        }
+    }
+}
