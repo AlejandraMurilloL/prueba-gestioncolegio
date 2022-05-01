117bb306d25b0dbb1a3f0b7e7b122dfbd59583d2
Maria Alejandra Murillo <malemurillo15@gmail.com>
2022-04-30 19:19:07 -0500

    Se crea entidad de profesor

---
+++ Backend/GestionColegio/Domain/Entities/Profesor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionColegio.Domain.Entities
+{
+    public class Profesor : Usuario
+    {
+        public Profesor(string identificacion, string nombre, string apellido) 
+            : base(identificacion, nombre, apellido)
+        {
+
+        }
+    }
+}
